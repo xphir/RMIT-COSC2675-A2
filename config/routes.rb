3a700023194919153512a4d78bc00270a03f3700
Elliot Schot <s3530160@student.rmit.edu.au>
2019-04-27 04:34:16 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get  '/signup',  to: 'users#new'
   root 'static_pages#home'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
   #get  '/help',    to: 'static_pages#help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
