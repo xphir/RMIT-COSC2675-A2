dd4ef52a6748c154719135ac5e67ac1d037454f2
Elliot Schot <s3530160@student.rmit.edu.au>
2019-04-27 06:36:34 +0000

    Finish user edit, update, index, and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name:  "John Snow",
+  email: "John.Snow@rmit.edu.au",
+  password:              "password",
+  password_confirmation: "password",
+  admin: true)
+
+99.times do |n|
+firstname = Faker::Name.first_name
+lastname = Faker::Name.last_name
+email = firstname + "." + lastname + "@rmit.edu.au"
+password = "password"
+User.create!(name:  firstname + " " + lastname,
+    email: email,
+    password:              password,
+    password_confirmation: password)
+end
